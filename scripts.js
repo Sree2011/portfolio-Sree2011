cda6204f0f6843a5d2454c154e3073d818877112
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-08-13 16:02:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,4 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow();
-
-    const nameCell = newRow.insertCell(0);
-    const emailCell = newRow.insertCell(1);
-
-    nameCell.textContent = name;
-    emailCell.textContent = email;
     alert('Form submitted!');
-
-    document.getElementById('myForm').reset();
 });
